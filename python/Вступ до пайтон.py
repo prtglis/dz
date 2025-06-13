0967e2509e885994a1d9d2a48a25037d5ad8f6fc
Tim <gorvinok@gmail.com>
2025-06-13 20:37:17 +0300

    initial commit

---
+++ python/Вступ до пайтон.py
@@ -0,0 +1,9 @@
+#завдання 1
+print("To be\nor not\nto be")
+#завдання 2
+print('"Life is what happens\n\twhen\n\tyou`re busy making other plans"\n\tJohn Lennon')
+#завдання 3
+# варіант тіки з символами табуляції
+print('"The best way to predict the future\n\tis to\n\t\tinvent it."\n\t\t\tAlan Kay')
+# більш естетичний варіант
+print('"The best way to predict the future\n   is to\n       invent it."\n\t   Alan Kay')
